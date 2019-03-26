4e65d79ee8030ac68d8c41779f4c21ea1aa051c7
sregnard <stanley.regnard@gmail.com>
2019-03-26 22:14:22 +0100

    ex07

---
+++ d01/ex07/rostring.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+
+function    ft_next_word($s, &$i)
+{
+    $word = "";
+    while ($s[$i] && $s[$i] != ' ')
+        $word .= $s[$i++];
+    while ($s[$i] && $s[$i] == ' ')
+        $i++;
+    return $word;
+}
+
+if (--$argc < 1)
+    return (0);
+$s = $argv[1];
+$i = 0;
+$first_word = ft_next_word($s, $i);
+while ($word = ft_next_word($s, $i))
+    echo $word . " ";
+echo $first_word . "\n";
+?>
