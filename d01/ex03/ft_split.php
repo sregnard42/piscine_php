a7c625a79c0636157d4714ce3f289174fd925fed
Stanley REGNARD <sregnard@e3r8p19.42.fr>
2019-03-26 18:29:16 +0100

    ex03

---
+++ d01/ex03/ft_split.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+
+function    ft_next_word($s, &$i)
+{
+    $word = "";
+    while ($s[$i] && $s[$i] != ' ')
+        $word .= $s[$i++];
+    while ($s[$i] && $s[$i] == ' ')
+        $i++;
+    return $word;
+}
+
+function    ft_split($s)
+{
+    $i = 0;
+    $array = array();
+    while ($word = ft_next_word($s, $i))
+        array_push($array, $word);
+    return $array;
+}
+?>
