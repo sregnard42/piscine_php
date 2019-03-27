336085c0c7b3e9eea7eb5a65b480b34c1b1f7993
Stanley REGNARD <sregnard@e3r8p19.42.fr>
2019-03-27 16:47:51 +0100

    ex08

--- d01/ex08/ft_is_sort.php
+++ d01/ex08/ft_is_sort.php
@@ -6,6 +6,9 @@ function ft_is_sort($tab)
     sort($sorted);
     $rsorted = $tab;
     rsort($rsorted);
+    print_r($tab);
+    print_r($sorted);
+    print_r($rsorted);
     return ($tab === $sorted || $tab === $rsorted);
 }
 
--- d01/ex08/main.php
+++ d01/ex08/main.php
@@ -1,9 +1,9 @@
 #!/usr/bin/php
 <?php
 include("ft_is_sort.php");
-$tab = array("!/@#;^", "42", "Hello World", "salut", "zZzZzZz");
-$tab[] = "Et qu’est-ce qu’on fait maintenant ?";
-if (ft_is_sort($tab))
+unset($argv[0]);
+$argv = array_merge($argv);
+if (ft_is_sort($argv))
 echo "Le tableau est trie\n";
 else
 echo "Le tableau n’est pas trie\n";
