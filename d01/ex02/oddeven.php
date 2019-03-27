b63f3085a47b9ead924f0c3b1f3669ac83d1eec6
Stanley REGNARD <sregnard@e3r8p19.42.fr>
2019-03-27 17:29:44 +0100

    ex02

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -2,11 +2,14 @@
 <?php
 $request = "Entrez un nombre: ";
 echo $request;
+echo $float;
 while (($input = trim(fgets(STDIN))) != NULL) {
-    if ($input - (int)$input != 0)
+    $float = strstr($input, ".");
+    if ($float == true && $input - (int)$input != 0)
         echo "'" . $input . "' n'est pas un chiffre\n";
     else if (is_numeric($input)) {
-        if (substr((int)$input, -1) % 2 == 0)
+        $nb = $float == true ? (int)$input : $input;
+        if (substr($nb, -1) % 2 == 0)
             echo "Le chiffre " . $input . " est Pair\n";
         else
             echo "Le chiffre " . $input . " est Impair\n";
