80bb8efb8a3ab98b2c12b635e57a80f3378eb7bd
Stanley REGNARD <sregnard@e3r8p19.42.fr>
2019-03-28 14:20:47 +0100

    ex03_tests

---
+++ d03/ex03/ex03_tests/del.sh
@@ -0,0 +1,1 @@
+ curl -c cook.txt 'http://e3r8p19.42.fr:8100/ex03/cookie_crisp.php?action=del&name=plat'
---
+++ d03/ex03/ex03_tests/get.sh
@@ -0,0 +1,1 @@
+curl -b cook.txt 'http://e3r8p19.42.fr:8100/ex03/cookie_crisp.php?action=get&name=plat'
