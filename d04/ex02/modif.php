8756b77c44c832b5d4a706704d37ad51a14a3347
Stanley REGNARD <sregnard@e3r7p18.42.fr>
2019-03-29 13:48:46 +0100

    ex02

---
+++ d04/ex02/modif.php
@@ -0,0 +1,47 @@
+<?php
+
+function ft_error($error)
+{
+    echo $error;
+    exit(0);
+}
+
+function user_exists($content, $login)
+{
+    if (!$content)
+        return (0);
+    foreach ($content as $existing_user)
+        if ($existing_user["login"] == $login) {
+        return (1);
+    }
+}
+
+function user_add()
+{
+    if ($submit != "OK" || !$login || !$passwd)
+        return (0);
+    if (!file_exists($dir)) {
+        mkdir($dir);
+        $content = [];
+    } else
+        $content = (file_exists($file)) ? unserialize(file_get_contents($file)) : [];
+    if (user_exists($content, $login))
+        return (0);
+    $user = [];
+    $passwd = hash("whirlpool", $passwd);
+    $user["login"] = $login;
+    $user["passwd"] = $passwd;
+    $content .= $user;
+    file_put_contents($file, serialize($content));
+    return (1);
+}
+
+$success = "OK\n";
+$error = "ERROR\n";
+$dir = "private/";
+$file = $dir . "passwd";
+
+if (!user_add($_POST["login"], $_POST["passwd"], $_POST["submit"], $dir, $file))
+    ft_error($error);
+echo $success;
+?>
