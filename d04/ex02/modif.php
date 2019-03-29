fb1accf995e633717bb8809cbee8f75b0ad68200
Stanley REGNARD <sregnard@e3r7p18.42.fr>
2019-03-29 15:39:09 +0100

    ex02

--- d04/ex02/modif.php
+++ d04/ex02/modif.php
@@ -6,34 +6,17 @@ function ft_error($error)
     exit(0);
 }
 
-function user_exists($content, $login)
+function find_and_replace(&$content, $login, $oldpw, $newpw)
 {
     if (!$content)
         return (0);
-    foreach ($content as $existing_user)
-        if ($existing_user['login'] == $login)
-           return (1);
+    foreach ($content as $index => $existing_user) {
+        if ($existing_user["login"] == $login && $existing_user["passwd"] == $oldpw) {
+            $content[$index]["passwd"] = $newpw;
+            return (1);
+        }
+    }
     return (0);
-}
-
-function user_add($login, $passwd, $submit, $dir, $file)
-{
-    if ($submit != "OK" || !$login || !$passwd)
-        return (0);
-    if (!file_exists($dir)) {
-        mkdir($dir);
-        $content = [];
-    } else
-        $content = (file_exists($file)) ? unserialize(file_get_contents($file)) : [];
-    if (user_exists($content, $login))
-        return (0);
-    $user = [];
-    $passwd = hash("whirlpool", $passwd);
-    $user["login"] = $login;
-    $user["passwd"] = $passwd;
-    $content[] = $user;
-    file_put_contents($file, serialize($content));
-    return (1);
 }
 
 $success = "OK\n";
@@ -41,7 +24,17 @@ $error = "ERROR\n";
 $dir = "../private/";
 $file = $dir . "passwd";
 
-if (!user_add($_POST["login"], $_POST["passwd"], $_POST["submit"], $dir, $file))
+$submit = $_POST["submit"];
+$login = $_POST["login"];
+$oldpw = $_POST["oldpw"];
+$newpw = $_POST["newpw"];
+if ($submit != "OK" || !$login || !$oldpw || !$newpw)
     ft_error($error);
+$oldpw = hash("whirlpool", $oldpw);
+$newpw = hash("whirlpool", $newpw);
+$content = unserialize(file_get_contents($file));
+if (!find_and_replace($content, $login, $oldpw, $newpw))
+    ft_error($error);
+file_put_contents($file, serialize($content));
 echo $success;
 ?>
