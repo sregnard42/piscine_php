1c1d8096b965e5c1e1f48c15f976827239933e38
Stanley REGNARD <sregnard@e3r7p18.42.fr>
2019-04-01 19:04:33 +0200

    ex19

--- d05/ex19/ex19.sql
+++ d05/ex19/ex19.sql
@@ -1,2 +1,3 @@
-SELECT DATEDIFF(MAX(last_projection), MIN(last_projection)) AS 'uptime'
+SELECT DATEDIFF(MAX(last_projection), MIN(release_date)) AS 'uptime'
 FROM film
+WHERE release_date != '0000-00-00'
